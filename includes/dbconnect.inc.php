6f9611f85eba8f66e6f3d6a9697581baf8d301c0
Marcel Riera <mrier011@fiu.edu>
2019-04-28 20:31:21 -0400

    Added postgreSQL connection

--- includes/dbconnect.inc.php
+++ includes/dbconnect.inc.php
@@ -1,9 +1,9 @@
 <?php
 
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "DBroot33125";
-$dbName = "HenryBooks";
+$dbServername = "ec2-174-129-10-235.compute-1.amazonaws.com";
+$dbUsername = "cedouazthnpjdx";
+$dbPassword = "7bdfe54c4b08f6d270c02762779d2165377465aa4da3dee076b433b6d01d6a44";
+$dbName = "dctt8pi4vphh31";
 
 $dbConn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
